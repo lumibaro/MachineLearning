79db8b1d2d00827368600131830a25d6ef53d018
John Wayne <john@debianz.vecindario>
2020-12-01 15:25:10 +0100

    Seguimos jugando1

---
+++ testing.py
@@ -0,0 +1,40 @@
+numero = 1200
+cadena1 = "Hola"
+cadena2= 'Adios'
+booleano = True
+flotante = 3.14158
+
+'''
+sintaxis de comentarios.
+Esta funcion devuelve una cadena.
+
+'''
+
+def suma(param1, param2):
+	variable = param1 + param2
+	print (variable)
+	return variable
+
+
+def suma(param1, param2):
+	return param1 + param2
+
+
+def suma(param1, param2):
+	return param1 + param2
+
+
+def divi(param1, param2):
+	res = 0
+	if param2 > 0:
+		res = param1 / param2
+	elese:
+		print("Menor a cero")
+	return res
+
+
+ret = suma(12,52)
+ret =div(12,0)
+print(ret)
+print(variable)
+
